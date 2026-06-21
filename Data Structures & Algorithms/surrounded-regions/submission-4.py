2f4a22cd7b0904733699b8cf374fa62687c6a206
Abhiraj Bhatta <abhirajbhatta@gmail.com>
2026-06-21 12:51:10 +0530

    Add: surrounded-regions - submission-4

---
+++ Data Structures & Algorithms/surrounded-regions/submission-4.py
@@ -0,0 +1,32 @@
+from _heapq import heapify
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        visited = set()
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c):
+            if (r<0 or c<0 or
+                r==rows or c==cols or
+                (r, c) in visited or
+                board[r][c]=="X"):
+                return
+            visited.add((r, c))
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
+            
+        for r in range(rows):
+            dfs(r, 0)
+            dfs(r, cols-1)
+        for c in range(cols):
+            dfs(0, c)
+            dfs(rows-1, c)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=="O" and (r, c) not in visited:
+                    board[r][c]="X"
+        
+
+                
