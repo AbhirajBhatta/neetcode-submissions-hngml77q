1c2d5be42c45be82428f2590d95665dc0e102c98
Abhiraj Bhatta <abhirajbhatta@gmail.com>
2026-07-27 22:42:50 +0530

    Add: pacific-atlantic-water-flow - submission-4

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+
+        pac, atl = set(), set()
+
+        def dfs(r, c, hashset, prev):
+            if (r<0 or c<0 or r==rows or c==cols or
+                (r, c) in hashset or heights[r][c]<prev):
+                return
+            hashset.add((r, c))
+            dfs(r+1, c, hashset, heights[r][c])
+            dfs(r-1, c, hashset, heights[r][c])
+            dfs(r, c+1, hashset, heights[r][c])
+            dfs(r, c-1, hashset, heights[r][c])
+        
+        for r in range(rows):
+            dfs(r, 0, pac, 0)
+            dfs(r, cols-1, atl, 0)
+        
+        for c in range(cols):
+            dfs(0, c, pac, 0)
+            dfs(rows-1, c, atl, 0)
+        res = []
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+        return res
+            
